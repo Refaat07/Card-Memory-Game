810d70d8df397cb3fcdc52d83d47754a7bb136a7
Keroles-Nady <Keroles_N@outlook.com>
2024-01-30 06:38:09 +0200

    Adding initiateTime function, and the initial code for scoring system (countdown timer, and number of flips)

--- js/index.js
+++ js/index.js
@@ -7,16 +7,29 @@ let matrix =[];
 
 let selectedCards = [];
 let isClickable = true;
+let stillPlaying = false;
 
 let trials;
 
 let NumOfMatches = 0;
 
+let timerInfo = document.getElementById('timer-info');
+let maxTime = 30;
+let timeLeft = maxTime;
+let timer;
+
+// Score Variables
+let flipInfo = document.getElementById('flips-info');
+
+
+
 window.onload = function() {
     trials = 0;
     shuffle();
     initializeMatrix();
-    setTimeout(addCardEventListeners,2000);
+    addCardEventListeners()
+    // setTimeout(addCardEventListeners,2000);
+    // initiateTime();
 }
 
 function shuffle() {
@@ -28,6 +41,14 @@ function shuffle() {
       }
     //   console.log(fruitsCards);
 
+}
+
+function initiateTime() {
+    if(timeLeft <= 0) {
+        return clearInterval(timer);
+    }
+    timeLeft--;
+    timerInfo.innerText = timeLeft;
 }
 
 function initializeMatrix () {
@@ -59,36 +80,42 @@ function hideCards() {
 }
 
 function selectCard() {
-    if (!isClickable) {
-        return;
+    if(!stillPlaying){
+        timer = setInterval(initiateTime, 1000);
     }
-  
-    let card = this;
-    let [row, col] = card.id.split('-');
-    let cardValue = matrix[row][col];
-  
-    // Check if the card is already selected or matched (add matched class when comparing cards)
-    if (card.classList.contains('matched')) {
-        return;
-    }
-    
-    if (selectedCards.some(selectedCard => selectedCard.card.id === card.id)) {
-        return;
-    }
-  
-    // reveal the selected card
-    card.src = "./images/" + cardValue + ".png";
-    console.log(card);
-    selectedCards.push({ card, value: cardValue });
-    console.log(selectedCards);
-  
-    // Check if two cards are selected
-    if (selectedCards.length === 2) {
-        isClickable = false; // Disable clicking during comparison
+    stillPlaying = true;
+    if(NumOfMatches < 8 && timeLeft > 0){
+        if (!isClickable) {
+            return;
+        }
+      
+        let card = this;
+        let [row, col] = card.id.split('-');
+        let cardValue = matrix[row][col];
+      
+        // Check if the card is already selected or matched
+        if (card.classList.contains('matched')) {
+            return;
+        }
+        
+        if (selectedCards.some(selectedCard => selectedCard.card.id === card.id)) {
+            return;
+        }
 
-        setTimeout(compareCards, 500); 
+        // Reveal the selected card
+        card.src = "./images/" + cardValue + ".png";
+        // console.log(card);
+        selectedCards.push({ card, value: cardValue });
+        // console.log(selectedCards);
+      
+        // Check if two cards are selected
+        if (selectedCards.length === 2) {
+            isClickable = false; // Disable clicking during comparison
+            setTimeout(compareCards, 500);
+        }
     }
-  }
+  
+}
 
 function addCardEventListeners() {
     let cards = document.querySelectorAll('.card');
@@ -97,25 +124,29 @@ function addCardEventListeners() {
     });
 }
 
+
 function compareCards() {
     let [card1, card2] = selectedCards;
     trials++;
-  
+    flipInfo.innerText = trials;
+    // console.log(trials);
     if (card1.value === card2.value) {
         // Matched cards
         card1.card.classList.add('matched');
         card2.card.classList.add('matched');
         NumOfMatches++;
         console.log(NumOfMatches);
-        
         if(NumOfMatches == 8){
-            console.log("Congrates!!");
+            console.log("Congrats!!");
+            clearInterval(timer);
+            
         }
         
     } else {
-        //Not matched, hide cards again
+        // Not matched, hide cards again
         card1.card.src = card2.card.src = "./images/question mark.png";
         card1.card.classList.add("shake");
+        // card2.card.src = "./images/question mark.png";
         card2.card.classList.add("shake");
     }
     setTimeout(() => {
@@ -127,3 +158,4 @@ function compareCards() {
     selectedCards = [];
     isClickable = true; // Enable clicking for the next turn
 }
+
